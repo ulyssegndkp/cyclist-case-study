1ef7085d96371e56aded387cdc084c923f77dac4
ulyssegndkp <ulysse.gnidokponou@gmail.com>
2025-12-20 16:49:43 -0500

    Update 02_core_metrics.sql

--- sql/02_core_metrics.sql
+++ sql/02_core_metrics.sql
@@ -1,1 +1,8 @@
+SELECT
+  user_type_norm AS user_type,
+  COUNT(*) AS total_rides
+FROM `valiant-monitor-476020-u8.Cyclist_Case_Study.v_trips_q1_clean`
+WHERE user_type_norm IN ('member', 'casual')
+GROUP BY 1
+ORDER BY total_rides DESC;
 
