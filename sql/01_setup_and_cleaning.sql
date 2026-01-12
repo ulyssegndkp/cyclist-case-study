f6ec754cfadcd5b201b829c702f7016eadc7a0ea
ulyssegndkp <ulysse.gnidokponou@gmail.com>
2026-01-12 18:21:19 -0500

    Update 01_setup_and_cleaning.sql

--- sql/01_setup_and_cleaning.sql
+++ sql/01_setup_and_cleaning.sql
@@ -1,44 +1,13 @@
--- 01_setup_and_cleaning.sql
--- Dataset: Q1 2019 Divvy trips
-
-SELECT
-  COUNT(*) AS total_rows,
-  COUNTIF(usertype IS NULL) AS null_usertype,
-  COUNTIF(start_time IS NULL) AS null_start_time,
-  COUNTIF(end_time IS NULL) AS null_end_time,
-  COUNTIF(tripduration IS NULL) AS null_tripduration
-FROM `valiant-monitor-476020-u8.Cyclist_Case_Study.Divvy_Trips_2019_Q1`;
-
-
--- Create a view (recommended) so Power BI and all queries reuse same logic
-CREATE OR REPLACE VIEW `valiant-monitor-476020-u8.Cyclist_Case_Study.v_trips_q1_clean` AS
+CREATE OR REPLACE TABLE `valiant-monitor-476020-u8.Cyclist_Case_Study.divvy_clean_q1` AS
 SELECT
   trip_id,
+  usertype,
   start_time,
   end_time,
-  bikeid,
-  usertype,
-  gender,
-  birthyear,
-  from_station_id,
+  TIMESTAMP_DIFF(end_time, start_time, MINUTE) AS ride_length_minutes,
+  EXTRACT(DAYOFWEEK FROM start_time) AS day_of_week,
+  EXTRACT(HOUR FROM start_time) AS start_hour,
   from_station_name,
-  to_station_id,
-  to_station_name,
-
-  -- Normalize user type
-  CASE
-    WHEN LOWER(usertype) IN ('subscriber', 'member', 'annual member') THEN 'member'
-    WHEN LOWER(usertype) IN ('customer', 'casual') THEN 'casual'
-    ELSE 'unknown'
-  END AS user_type_norm,
-
-  -- Duration in minutes:
-  (tripduration / 60.0) AS ride_minutes,
-
-  -- Time features
-  EXTRACT(HOUR FROM start_time) AS start_hour
+  to_station_name
 FROM `valiant-monitor-476020-u8.Cyclist_Case_Study.Divvy_Trips_2019_Q1`
-WHERE start_time IS NOT NULL
-  AND end_time IS NOT NULL
-  AND tripduration IS NOT NULL
-  AND tripduration > 0;
+WHERE TIMESTAMP_DIFF(end_time, start_time, MINUTE) > 0;
