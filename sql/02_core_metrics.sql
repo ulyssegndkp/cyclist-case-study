79a3a44c0cda0272c406ee8fc9452891cee4fcf0
ulyssegndkp <ulysse.gnidokponou@gmail.com>
2026-01-12 18:21:53 -0500

    Update 02_core_metrics.sql

--- sql/02_core_metrics.sql
+++ sql/02_core_metrics.sql
@@ -1,8 +1,26 @@
 SELECT
-  user_type_norm AS user_type,
-  COUNT(*) AS total_rides
-FROM `valiant-monitor-476020-u8.Cyclist_Case_Study.v_trips_q1_clean`
-WHERE user_type_norm IN ('member', 'casual')
-GROUP BY 1
-ORDER BY total_rides DESC;
+usertype,
+COUNT (*) AS total_rides
+FROM 'valiant-monitor-476020-u8.Cyclist_Case_Study.divvy_clean_q1
+GROUP BY
+usertype
+ORDER
+BY
+total_rides DESC;
+-- Optional: add % share for your narrative (still Part 4.1)
+WITH totals AS (
+SELECT COUNT (*) AS all_rides
+FROM valiant-monitor-476020-u8.Cyclist_Case_Study.divvy_clean_q1'
+SELECT
+usertype,
+COUNT (*) AS total_rides,
+ROUND (100 * COUNT (*) / (SELECT all_rides FROM totals), 2) AS pct_of_total
+FROM
+*valiant-monitor-476020-u8.Cyclist_Case_Study divvy_clean_q1'
+GROUP
+BY
+usertype
+ORDER
+BY
+total_rides DESC;
 
